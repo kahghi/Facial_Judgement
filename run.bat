5ed53fb4695c64386c384716da93f00d4636536e
kahghi <kahghilim@gmail.com>
2022-05-08 22:09:40 +0800

    removed local path

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 @echo off
-set CONDAPATH=C:\Users\KahGhi\anaconda3
-set ENVNAME=aiap_wk8
+set CONDAPATH=<path to Conda>
+set ENVNAME=<envname>
 set ENVPATH=%CONDAPATH%\envs\%ENVNAME%
 call %CONDAPATH%/Scripts/activate.bat %ENVPATH%
 python -m src.app
